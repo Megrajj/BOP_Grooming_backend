9c3b5ed156a85565d2abfd4d9ca64942a812cff7
Suman Neupane <Megraj58447@gmail.com>
2024-11-12 13:13:02 +1300

    ssss

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
     method:["GET", "POST","PUT","DELETE"],
     credentials:true
     
-}));
+}))
 app.use(bodyParser.json());
 app.use('/api', bookingRoutes);
 
@@ -28,7 +28,7 @@ app.get('/api/service', (req, res) => {
     res.json(serviceData);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
